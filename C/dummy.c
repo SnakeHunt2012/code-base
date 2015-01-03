4ad37742b8fa647ea614cd1ab3cffbcd6f58b831
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-01-03 12:14:25 +0800

    From 《The C Programming Language》 Page 70
    Add function: dummy()
        does nothing and returns nothing

---
+++ C/dummy.c
@@ -0,0 +1,8 @@
+/**************************************
+ * The minimal function, which does   *
+ * nothing and returns nothing. A     *
+ * do-nothing function like this is   *
+ * sometimes useful as a place holder *
+ * during program development.        *
+ **************************************/
+dummy() {}
