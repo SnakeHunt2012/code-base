ee403c3c3d64b1b4c7312f04d3af9f6bb682a392
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-10-28 23:12:17 +0800

    From 《Algorithms in C (Parts 1-4)》 Program 6.4 Bubble sort

---
+++ C/sort/bubble-sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B))
+#define exch(A, B) { Item t = A; A = B; B = t; }
+#define compexch(A, B) if (less(B, A)) exch(A, B)
+
+/* bubble sort */
+void sort(Item data[], int left, int right)
+{
+    int i, j;
+    for (i = left; i < right; ++i)
+        for (j = right; j > i; --j)
+            compexch(data[j - 1], data[j]);
+}
+
+main(int argc, char *argv[])
+{
+    int i, N = atoi(argv[1]);
+    int *data = malloc(N * sizeof(int));
+    for (i = 0; i < N; ++i)
+        data[i] = 10 * (1.0 * rand() / RAND_MAX);
+    for (i = 0; i< N; ++i) printf("%3d", data[i]);
+    putchar('\n');
+    sort(data, 0, N - 1);
+    for (i = 0; i< N; ++i) printf("%3d", data[i]);
+    putchar('\n');
+    return 0;
+}
