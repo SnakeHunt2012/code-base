d72f36712c5e4da179d2e600f700f01df0c7fb1c
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-12-02 16:39:08 +0800

    From 《Algorithms in C (Parts 1-4)》 8.6 Linked-list merge and 8.7 Top-down list mergesort.

---
+++ C/sort/list-top-down-merge-sort.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node *link;
+struct node {
+    int value;
+    link next;
+};
+
+link merge(link a, link b)
+{
+    struct node head;
+    link index = &head;
+    while (a && b)
+        if (a->value < b->value) {
+            index->next = a;
+            index = index->next;
+            a = a->next;
+        } else {
+            index->next = b;
+            index = index->next;
+            b = b->next;
+        }
+    index->next = a ? a : b;
+    return head.next;
+}
+
+link merge_sort(link list)
+{
+    link a, b;
+    if (!list || !list->next)
+        return list;
+
+    a = list;
+    b = list->next;
+    while (b && b->next) {
+        list = list->next;
+        b = b->next->next;
+    }
+    b = list->next;
+    list->next = NULL;
+    return merge(merge_sort(a), merge_sort(b));
+}
+
+int main(int argc, char *argv[])
+{
+    int i, length = atoi(argv[1]);
+    link index, list = malloc(length * sizeof(*list));
+
+    /* initialize */
+    for (i = 0; i < length; ++i) {
+        list[i].value = 10 * (1.0 * rand() / RAND_MAX);
+        list[i].next = (i == length - 1) ? NULL : &list[i + 1];
+    }
+    /* print original list */
+    for (i = 0; i < length; ++i)
+        printf("%3d", list[i]);
+    putchar('\n');
+    
+    list = merge_sort(list);
+
+    /* print merged list */
+    for (index = list; index; index = index->next)
+        printf("%3d", index->value);
+    putchar('\n');
+}
